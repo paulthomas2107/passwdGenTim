ab2fe2cb45db27d8ac1a9d503388a60c481c34a7
Paul Thomas <paulthomas2107@gmail.com>
2023-02-14 16:07:46 +0000

    first commit

---
+++ password_generator.py
@@ -0,0 +1,36 @@
+import random
+import string
+
+def generate_password(min_length, numbers=True, special_charactera=True):
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+
+    characters = letters
+    if numbers:
+        characters += digits
+    if special_charactera:
+        characters += special
+    
+    pwd = ""
+    meets_criteria = False
+    has_number = False
+    has_special = False
+
+    while not meets_criteria or len(pwd) < min_length:
+        new_char = random.choice(characters)
+        pwd += new_char
+
+        if new_char in digits:
+            has_number = True
+        elif new_char in special:
+            has_special = True
+        
+        meets_criteria = True
+        if numbers:
+            meets_criteria = has_number
+        if special_charactera:
+            meets_criteria = meets_criteria and has_special
+
+
+generate_password(10) 
