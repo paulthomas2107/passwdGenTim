2f884140f8c8b15ca6a9cd8cd3e990389de724cd
Paul Thomas <paulthomas2107@gmail.com>
2023-02-14 19:35:45 +0000

    w

--- password_generator.py
+++ password_generator.py
@@ -28,7 +28,7 @@ def generate_password(min_length, numbers=True, special_characters=True):
         
         meets_criteria = True
         if numbers:
-            meets_criteria = has_number50
+            meets_criteria = has_number
         if special_characters:
             meets_criteria = meets_criteria and has_special
 
